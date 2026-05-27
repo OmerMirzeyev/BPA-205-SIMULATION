c500d7bc9ac9245d5c742e95893f62093de1629e
OmerMirzeyev <mirzeyevomer1@gmail.com>
2026-05-27 19:39:05 +0400

    Delete Areas/Admin directory

--- Areas/Admin/ViewModels/Category/CreateCategoryVM.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Simulation_2.Areas.Admin.ViewModels.Category
-{
-    public class CreateCategoryVM
-    {
-        public string CategoryName { get; set; }
-    }
-}
--- Areas/Admin/ViewModels/Category/UpdateCategoryVM.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Simulation_2.Areas.Admin.ViewModels.Category
-{
-    public class UpdateCategoryVM
-    {
-        public int Id { get; set; }
-        public string CategoryName { get; set; }
-    }
-}
