8ac35bd47ebb629b237ef2664b3f9fef9379fcf5
Omer2121212121 <mirzeyevomer1@gmail.com>
2026-06-05 18:14:51 +0400

    Sim7

--- Sim7/Areas/Admin/Controllers/MemberController.cs
+++ Sim7/Areas/Admin/Controllers/MemberController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Sim7.Areas.Admin.ViewModels.Member;
 using Sim7.DAL;
@@ -9,7 +10,7 @@ using System.Threading.Tasks;
 namespace Sim7.Areas.Admin.Controllers
 {
     [Area("Admin")]
-
+    [Authorize(Roles = "Admin")]
     public class MemberController : Controller
     {
         private readonly AppDbContext _db;
